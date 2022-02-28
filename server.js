cb116b3916ead881ec113dd931f5b4e26935cfbb
Bram-ter <bram.tersteeg2@hva.nl>
2022-02-28 16:41:56 +0100

    change various things

--- server.js
+++ server.js
@@ -10,25 +10,31 @@ const info = {
   favorite: "Chess"
 }
 
+/* Set template engine */
 app.set("view engine", "ejs");
 
+/* Middleware */
 app.use(express.static("src"));
 
+/* Get profile as homepage */
 app.get('/', (req, res) => {
   res.render("profile", {
     pageTitle: `profile`,
   })
 })
 
+/* If no routes give response, show 404 Page */
 app.use((req, res, next) => {
   res.status(404).send(`404 This page could not be found`)
 })
 
+/* If something breaks in the code, show this message */
 app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).send(`Something broke!`)
 })
 
+/* Shows the app is listening, and also returns the port with it */
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
