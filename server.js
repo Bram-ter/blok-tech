a46038b928a1b83ab5f0dcac74f58e3b582950ad
Bram-ter <bram.tersteeg2@hva.nl>
2022-03-14 12:37:43 +0100

    update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const res = require('express/lib/response');
 
 const app = express()
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 /* constante */
 let db = null;
