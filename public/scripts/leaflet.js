0b71344123f794f3fab052386c8725bb3abd1fa9
Bram-ter <bram.tersteeg2@hva.nl>
2022-03-15 13:13:01 +0100

    add component text

--- public/scripts/leaflet.js
+++ public/scripts/leaflet.js
@@ -1,3 +1,4 @@
+/* Init leaflet map */
 var map = L.map('map').setView([52.3724724, 4.9006668], 18);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnJhbXRlciIsImEiOiJjbDBqb3hraXowY2l0M2NxdDdtbHl1Y2hlIn0.PWAp8l3jQZhXJYMrG-_v2Q', {
@@ -8,6 +9,8 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'your.mapbox.access.token'
 }).addTo(map);
 
+
+/* Init geolation */
 navigator.geolocation.getCurrentPosition(position => {
     const { latitude, longitude } = position.coords;
     // Do something cool with latitude, longitude
